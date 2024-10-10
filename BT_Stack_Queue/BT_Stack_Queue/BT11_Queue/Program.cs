a0ea8ba28be23e9883b3d7048bc27153102fe6de
Harick <117738664+Harick2005@users.noreply.github.com>
2024-10-10 22:10:16 +0700

    Add files via upload

---
+++ BT_Stack_Queue/BT_Stack_Queue/BT11_Queue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT11_Queue
+{
+    class Program
+    {
+        //Ham kiem tra so hoan hao
+        public static bool KiemTraSoHoanHao(int n)
+        {
+            int TongUocSo = 0;
+            for(int i=1;i<n;i++)
+            {
+                if (n % i == 0) TongUocSo += i;
+            }
+            return (TongUocSo == n);
+        }
+        static void Main(string[] args)
+        {
+            //Khai bao bien
+            Queue<int> queue = new Queue<int>();
+            int sopt = 0;
+
+            Console.WriteLine("Chuong trinh minh hoa hang doi");
+            Console.Write("Nhap so phan tu: ");
+            sopt = int.Parse(Console.ReadLine());
+            for(int i=1;i<=sopt;i++)
+            {
+                Console.Write("Phan tu thu {0}: ", i);
+                int value = int.Parse(Console.ReadLine());
+                queue.Enqueue(value);
+            }
+            int Dem = 0;
+            Console.WriteLine("\nCac so hoan hao co trong danh sach: ");
+            while(queue.Count>0)
+            {
+                int value = queue.Dequeue();
+                if(KiemTraSoHoanHao(value))
+                {
+                    Console.Write("{0}\t", value);
+                    Dem++;
+                }
+            }
+            Console.WriteLine("\nCo {0} so hoan hao trong danh sach", Dem);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BT_Stack_Queue/BT_Stack_Queue/BT14_Stack/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT14_Stack
+{
+    class Program
+    {
+        //Ham kiem tra bieu thuc dau ngoac co hop le hay khoong
+        public static bool KiemTraBieuThuc(string bt)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach(char c in bt)
+            {
+                if (c != ')' && c != ']' && c != '}')
+                    stack.Push(c);
+                else if ((c == ')' && stack.Pop()!='(') || (c == ']' && stack.Pop()!='[') || (c == '}' && stack.Pop()!='{'))
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chuong trinh minh hoa Stack kiem tra bieu thuc dau ngoac");
+            Console.Write("Nhap chuoi bieu thuc: ");
+            string bieuthuc = Console.ReadLine();
+            if (KiemTraBieuThuc(bieuthuc))
+                Console.WriteLine("Bieu thuc hop le!");
+            else
+                Console.WriteLine("Bieu thuc khong hop le!!");
+            Console.ReadLine();
+        }
+    }
+}
